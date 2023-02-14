06e922604f666b9ab03ad2d1769deeef90722f63
Larry Dotter <112337275+Larry-Dotter@users.noreply.github.com>
2023-02-14 20:38:47 +0100

    CB-WEB-PHP-14.02.2023

--- PHP, JS, USW/2023-02-07/config.php
+++ PHP, JS, USW/2023-02-07/config.php
@@ -1,3 +1,3 @@
 <?php
-   define('BASE_URL', 'http://localhost:63342/codersbay/2023-02-07/');
+   define('BASE_URL', 'http://localhost:63342/CB-WEB-PRAKTISCH/PHP,%20JS,%20USW/2023-02-07/');
  ?>
--- PHP, JS, USW/2023-02-07/controller/ToDo.php
+++ PHP, JS, USW/2023-02-07/controller/ToDo.php
@@ -9,27 +9,50 @@
 
 
     public function __construct() {
-
+    if(!isset($_SESSION['auth_status'])) header("Location: index.php");
+    $this->toDoModel =new ToDoModel();
     }
 
     public function getToDo() :array {
-
+  return $this->toDoModel->fetchToDo();
     }
 
     public function newEntry(array $data) {
+    $headline = stripcslashes(strip_tags($data['headline']));
+    $description = stripcslashes((strip_tags($data['description']))); // FEHLER!!
 
+      $Error = array(
+          'headline' => '',
+        'description' => ''
+      );
+
+      if(strlen($headline) <1) {
+        $Error['description'] = 'need mode characters';
+        return $Error;
+      }
+      $saveIt = array(
+          'description' => $description,
+          'headline' => $headline
+      );
+      $Response = $this->toDoModel->createToDo($saveIt);
+      todoHelper($Response);
+      return true;
     }
 
     public function singleEntry($id) {
-
+    $Response = $this->toDoModel->singleEntry($id);
+    return $Response;
     }
 
-    public function updateEntry(array $data) {
 
+    public function updateEntry(array $data) {
+      $Response = $this->toDoModel->updateSingleEntry($data);
+      todoHelper($Response);
     }
 
     public function deleteEntry($id) {
-
+    $Response = $this->toDoModel->deleteEntry($id);
+    todoHelper($Response);
     }
 
 
